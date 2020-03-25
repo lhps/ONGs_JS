f271f16bb82ea977954197802a8a464883b2f712
Lucas Pinho <pinho@MacBook-Pro-de-Lucas.local>
2020-03-25 15:03:51 -0300

    Página Incident criada e estilisada

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -11,7 +11,7 @@ export default function Profile() {
       <header>  
         <img src={logoImg} alt="Be The Hero"/>
         <span>Bem Vinda, APAD</span>
-        <Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
+        <Link className="button" to="/incident/new">Cadastrar novo caso</Link>
         <button type="button">
           <FiPower size={18} color="#E02041"/>
         </button>
