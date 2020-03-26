dd0999856da5038bf1adb507079c4809711796b7
Lucas Pinho <pinho@MacBook-Pro-de-Lucas.local>
2020-03-26 15:09:18 -0300

    Tela Incidents Mobile Estilisada

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {View, Image, Text} from 'react-native';
+import { Feather } from '@expo/vector-icons'
+import {View, FlatList, Image, Text, TouchableOpacity} from 'react-native';
 
 import logoImg from '../../assets/logo.png';
 import styles from './styles';
@@ -19,6 +20,33 @@ export default function Incidents() {
       <Text style={styles.description}>
         Escolha um dos casos abaixo e salve o dia.
       </Text>
+
+      <FlatList 
+        style={styles.incidentList}
+        data={[1,2,3, 4, 5, 6, 7]}
+        keyExtractor={incident => String(incident)}
+        showsVerticalScrollIndicator={false}
+        renderItem={() => (
+          <View style={styles.incident}> 
+            <Text style={styles.incidentProperty}>ONG:</Text>
+            <Text style={styles.incidentValue}>APAD</Text>
+
+            <Text style={styles.incidentProperty}>CASO:</Text>
+            <Text style={styles.incidentValue}>Cadelinha atropelada</Text>
+
+            <Text style={styles.incidentProperty}>VALOR:</Text>
+            <Text style={styles.incidentValue}>R$ 120,00</Text>
+
+            <TouchableOpacity
+            style={styles.detailsButton} 
+            onPress={() => {}}>
+              <Text style={styles.detailsButtonText}>
+                Ver mais detalhes
+              </Text>
+              <Feather name="arrow-right" size={16} color="#e02041"/>
+            </TouchableOpacity>
+          </View>
+        )}/>
 
     </View>
   );
