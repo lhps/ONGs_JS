666d3ed612a1c69bed9578802745b45fa0e4aedd
Lucas Pinho <pinho@MacBook-Pro-de-Lucas.local>
2020-03-27 09:33:33 -0300

    Adicionando validação no backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const {errors} = require('celebrate');
+
 const routes = require('./routes');
 
 const app = express();
@@ -8,7 +10,7 @@ app.use(cors());
 
 app.use(express.json());
 app.use(routes);
-
+app.use(errors());
 
 
 app.listen(3333);
